c013fbec3d9b3ae55074e3a17571c4fcd8f5e3bc
Ruiz Sanchez Carlos <carlos.ruizsanchez@unamur.be>
2020-04-30 11:27:44 +0200

    delete build sh

--- build-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC2006
-# shellcheck disable=SC2086
-dir=`dirname "$(realpath $0)"`
-name=unamurcsfaculty/pwsh-msteams
-version=latest
-
-docker image build -t ${name}:${version} ${dir}
